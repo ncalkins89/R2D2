b712662ba93afb60e6985c0c02f8406ab7dd1f13
calkinsn <calkinsn@amazon.com>
2018-10-10 22:58:35 -0700

    attempting to use button callback to execute script

--- bin/app.py
+++ bin/app.py
@@ -2,22 +2,26 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
+import lib.macros.bedtime as bedtime
 
 app = dash.Dash()
 
 app.layout = html.Div([
     html.Button('Click Me', id='button'),
-    html.Div(id='my-div')
+    html.Div(id='my-div'),
+    html.P(id='placeholder')
 ])
 
 
 @app.callback(
-    Output(component_id='my-div', component_property='children'),
+    Output(component_id='placeholder', component_property='children'),
     [Input('button', 'n_clicks')]
 )
 def update_output_div(n_clicks):
-    return 'You\'ve clicked {} times'.format(n_clicks)
+    bedtime()
+    return None
 
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0')
+    # app.run_server(host='0.0.0.0')
+    app.run_server()
