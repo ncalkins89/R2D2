0273a7896e37a392c7594255d6a5186f20d1bb87
calkinsn <calkinsn@amazon.com>
2018-10-10 23:33:15 -0700

    removed empty lib folder

--- lib/pc.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-
-def remote_shutdown_desktop():
-    subprocess.call("sudo net rpc shutdown -f -t 0 -C \'SHUTTIN THIS BIATCH DOWN! (via the Raspberry Pi)\' -U ncalk%Lateralus789 -I 192.168.0.20", shell=True)
