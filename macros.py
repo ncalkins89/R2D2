f190e972cb004892e4163e7805c34526b8781b5a
ncalkins89 <ncalkins89@gmail.com>
2018-10-15 00:31:03 -0700

    updated bedtime macro with night scene for hue

--- macros.py
+++ macros.py
@@ -6,7 +6,7 @@ import pc
 def bedtime():
     harmony.turn_off_current_activity()
     hue.turn_off_all_lights()
-    hue.turn_on_nightlight()
+    hue.turn_on_master_bedroom_night_scene()
     pc.remote_shutdown_desktop()
 
 
