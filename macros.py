c39b0409dddf8bec2dd408372d027ec4c612139e
calkinsn <calkinsn@amazon.com>
2018-10-21 18:30:30 -0700

    added subwoofer to macros

--- macros.py
+++ macros.py
@@ -1,16 +1,16 @@
 import harmony
 import hue
 import pc
-
-
-def bedtime():
-    harmony.turn_off_current_activity()
-    hue.turn_off_all_lights()
-    hue.turn_on_nightlight()
-    pc.remote_shutdown_desktop()
+import tplink
 
 
 def turn_everything_off():
     harmony.turn_off_current_activity()
+    tplink.subwoofer.turn_off()
     hue.turn_off_all_lights()
     pc.remote_shutdown_desktop()
+
+
+def bedtime():
+    turn_everything_off()
+    hue.turn_on_nightlight()
